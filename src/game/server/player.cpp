3ac1e35b22a4374ae34cdedcbec4f471474d4383
RemakePower <diandaokui@qq.com>
2023-09-18 21:43:53 +0800

    fix some /* problem */

--- src/game/server/player.cpp
+++ src/game/server/player.cpp
@@ -228,7 +228,7 @@ void CPlayer::Snap(int SnappingClient)
 	pPlayerInfo->m_Latency = SnappingClient == -1 ? m_Latency.m_Min : GameServer()->m_apPlayers[SnappingClient]->m_aActLatency[m_ClientID];
 	pPlayerInfo->m_ClientID = id;
 	pPlayerInfo->m_Score = m_Score;
-	pPlayerInfo->m_Team = (m_ClientID == SnappingClient ? m_Team : 0); // do not snap bot and spectator in scoreboard
+	pPlayerInfo->m_Team = (m_ClientID == SnappingClient ? m_Team : 0); // snap other spectator to team 0
 	pPlayerInfo->m_Local = 0;
 	
 	if (m_ClientID == SnappingClient)
@@ -308,7 +308,7 @@ void CPlayer::SnapBot(int SnappingClient)
 	pPlayerInfo->m_Latency = 0;
 	pPlayerInfo->m_ClientID = id;
 	pPlayerInfo->m_Score = m_Score;
-	pPlayerInfo->m_Team = 2; // do not snap bot and spectator in scoreboard
+	pPlayerInfo->m_Team = 2; // do not snap bot in scoreboard
 	pPlayerInfo->m_Local = 0;
 	
 	CNetObj_DDNetPlayer *pDDNetPlayer = static_cast<CNetObj_DDNetPlayer *>(Server()->SnapNewItem(NETOBJTYPE_DDNETPLAYER, id, sizeof(CNetObj_DDNetPlayer)));
